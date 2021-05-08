a3049e218ada09f0c0cb5c5a0a6eeb9c8efc4a1f
Justin Goats <justingoats@yahoo.com>
2021-05-07 17:18:47 -0700

    created models for movies ans users collections

---
+++ models.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+let movieScheme = mongoose.Schema({
+    title : {type : String , required : true},
+    description : {type : String , required : true},
+    genre : {name : String, description : String},
+    director : {name : String , bio : String},
+    imageURL : String,
+    featured : Boolean
+});
+
+let userSchema = mongoose.Schema({
+    username: {type: String, required: true},
+    password: {type: String, required: true},
+    email: {type: String},
+    birthday: Date,
+    favoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
+  });
+
+  let Movie = mongoose.model('Movie', movieSchema , "movies");
+  let User = mongoose.model('User', userSchema , "users");
+
+  module.exports.Movie = Movie;
+  module.exports.User = User;
