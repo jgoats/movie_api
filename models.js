0b050859c8a9589988327c71de52f63207f2d8a4
Justin Goats <justingoats@yahoo.com>
2021-05-23 16:33:10 -0700

    commit to master

--- models.js
+++ models.js
@@ -2,32 +2,32 @@ const mongoose = require("mongoose");
 const bcrypt = require('bcrypt');
 
 let movieSchema = mongoose.Schema({
-    title : {type : String , required : true},
-    description : {type : String , required : true},
-    genre : {name : String, description : String},
-    director : {name : String , bio : String},
-    imageURL : String,
-    featured : Boolean
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  genre: { name: String, description: String },
+  director: { name: String, bio: String },
+  imageURL: String,
+  featured: Boolean
 });
 
 let userSchema = mongoose.Schema({
-    username: {type: String, required: true},
-    password: {type: String, required: true},
-    email: {type: String},
-    birthday: Date,
-    favoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
-  });
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  email: { type: String },
+  birthday: Date,
+  favoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
+});
 
-  userSchema.statics.hashPassword = (password) => {
-    return bcrypt.hashSync(password, 10);
-  };
-  
-  userSchema.methods.validatePassword = function(password) {
-    return bcrypt.compareSync(password, this.password);
-  };
+userSchema.statics.hashPassword = (password) => {
+  return bcrypt.hashSync(password, 10);
+};
 
-  let Movie = mongoose.model('Movie', movieSchema , "movies");
-  let User = mongoose.model('User', userSchema , "users");
+userSchema.methods.validatePassword = function (password) {
+  return bcrypt.compareSync(password, this.password);
+};
 
-  module.exports.Movie = Movie;
-  module.exports.User = User;
+let Movie = mongoose.model('Movie', movieSchema, "movies");
+let User = mongoose.model('User', userSchema, "users");
+
+module.exports.Movie = Movie;
+module.exports.User = User;
