795748de083e2e45762554142d99644ea4f22891
Justin Goats <justingoats@yahoo.com>
2021-05-23 19:08:36 -0700

    using even older version on index.js to fix connection issues with mongodb cluster

--- index.js
+++ index.js
@@ -5,34 +5,26 @@ const mongoose = require("mongoose");
 const Models = require('./models.js');
 const Movies = Models.Movie;
 const Users = Models.User;
-const passport = require('passport');
-const cors = require('cors');
-const { check, validationResult } = require('express-validator');
-const { Console } = require("console");
-app.use(bodyParser.json());
-require('./passport');
-app.use(cors());
 
-//express is available inside the ./auth file 
-let auth = require('./auth')(app);
-mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
-mongoose.connection.on("connected", () => {
-  console.log("mongoose is connected!!!");
-})
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb://localhost:27017/myFLIXDB', { useNewUrlParser: true, useUnifiedTopology: true });
+
 app.get("/", (request, response) => {
   response.send("welcome to the myFLIX API");
 });
 
 app.get("/movies", (request, response) => {
-  Movies.find().then((movies) => {
-    response.status(201).json(movies)
-  }).catch((err) => {
-    console.log(err);
-    response.status(500).send(err)
-  });
-});
+  Movies.find()
+    .then((movies) => {
+      response.status(201).json(movies)
+    }).catch((err) => {
+      console.log(err);
+      response.status(500).send(err)
+    })
+})
 
-app.get('/movies/:movie', passport.authenticate('jwt', { session: false }), (req, res) => {
+app.get('/movies/:movie', (req, res) => {
   Movies.findOne({ title: req.params.movie })
     .then((movies) => {
       res.json(movies);
@@ -43,7 +35,7 @@ app.get('/movies/:movie', passport.authenticate('jwt', { session: false }), (req
     });
 });
 
-app.get("/movies/:movie/genre", passport.authenticate('jwt', { session: false }), (req, res) => {
+app.get("/movies/:movie/genre", (req, res) => {
   Movies.findOne({ title: req.params.movie })
     .then((movie) => {
       res.json(movie.genre);
@@ -54,7 +46,7 @@ app.get("/movies/:movie/genre", passport.authenticate('jwt', { session: false })
     });
 });
 
-app.get("/movies/:movie/director", passport.authenticate('jwt', { session: false }), (req, res) => {
+app.get("/movies/:movie/director", (req, res) => {
   Movies.findOne({ title: req.params.movie })
     .then((movie) => {
       res.json(movie.director)
@@ -65,19 +57,7 @@ app.get("/movies/:movie/director", passport.authenticate('jwt', { session: false
     });
 });
 
-app.post('/users', [
-  check('username', 'username is required').isLength({ min: 5 }),
-  check('username', 'username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
-  check('password', 'password is required').not().isEmpty(),
-  check('email', 'email does not appear to be valid').isEmail()
-], (req, res) => {
-  // check the validation object for errors
-  let errors = validationResult(req);
-
-  if (!errors.isEmpty()) {
-    return res.status(422).json({ errors: errors.array() });
-  }
-  let hashedPassword = Users.hashPassword(req.body.password);
+app.post('/users', (req, res) => {
   Users.findOne({ username: req.body.username })
     .then((user) => {
       if (user) {
@@ -86,7 +66,7 @@ app.post('/users', [
         Users
           .create({
             username: req.body.username,
-            password: hashedPassword,
+            password: req.body.password,
             email: req.body.email,
             birthday: req.body.birthday
           })
@@ -96,13 +76,10 @@ app.post('/users', [
             res.status(500).send('Error: ' + error);
           })
       }
-    }).catch((error) => {
-      console.error(error);
-      res.status(500).send('Error: ' + error);
-    });
+    })
 });
 
-app.put('/users/:Username', passport.authenticate('jwt', { session: false }), (req, res) => {
+app.put('/users/:Username', (req, res) => {
   Users.findOneAndUpdate({ username: req.params.Username }, {
     $set:
     {
@@ -123,24 +100,9 @@ app.put('/users/:Username', passport.authenticate('jwt', { session: false }), (r
     });
 });
 
-app.post('/users/:Username/favorites/:MovieID', passport.authenticate('jwt', { session: false }), (req, res) => {
+app.post('/users/:Username/favorites/:MovieID', (req, res) => {
   Users.findOneAndUpdate({ username: req.params.Username }, {
     $addToSet: { favoriteMovies: req.params.MovieID }
-  },
-    { new: true },
-    (err, updatedUser) => {
-      if (err) {
-        console.error(err);
-        res.status(500).send('Error: ' + err);
-      } else {
-        res.json(updatedUser);
-      }
-    });
-});
-
-app.delete('/users/:Username/deleteFavoriteMovie/:MovieID', passport.authenticate('jwt', { session: false }), (req, res) => {
-  Users.findOneAndUpdate({ username: req.params.Username }, {
-    $pull: { favoriteMovies: req.params.MovieID }
   },
     { new: true },
     (err, updatedUser) => {
@@ -172,11 +134,11 @@ app.delete('/users/:Username', (req, res) => {
 
 
 app.use((err, request, response, next) => {
-  console.error(err.stack);
+  console.error(error.stack);
   response.status(500).send('Something broke!');
 });
 
-const port = process.env.PORT || 8080;
-app.listen(port, '0.0.0.0', () => {
-  console.log('Listening on Port ' + port);
-});
+app.listen(8080, (err) => {
+  if (err) { console.log(err.json()) }
+  console.log("running on port 8080");
+})
