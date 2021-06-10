19b6a57395813c4625417ea0817d78c578f1725d
Justin Goats <justingoats@yahoo.com>
2021-06-09 22:49:39 -0700

    fixing authentication bug

--- passport.js
+++ passport.js
@@ -23,7 +23,7 @@ passport.use(new localStrategy(
         return done(null, false, 'Incorrect username...');
       }
       if (!user.validatePassword(password)) {
-        return done(null, false, 'Incorrect password...');
+        return done(null, false, `Incorrect password... ${password}`);
       }
 
       // console.log('Finished');
