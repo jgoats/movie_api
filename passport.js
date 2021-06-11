d3fe5f87ffaa3c8c75535e94e8214c7e6d2bff0d
Justin Goats <justingoats@yahoo.com>
2021-06-10 20:51:29 -0700

    commit to master

--- passport.js
+++ passport.js
@@ -8,30 +8,29 @@ const JWTStrategy = passportJWT.Strategy;
 const ExtractJWT = passportJWT.ExtractJwt;
 
 //http authentication strategy
-passport.use(new LocalStrategy({
-  usernameField: 'username',
-  passwordField: 'password'
-}, (username, password, callback) => {
-  Users.findOne({ username: username }, (error, user) => {
-    if (error) {
-      console.log(error);
-      return callback(error);
-    }
+passport.use(new localStrategy(
+  {
+    usernameField: 'username',
+    passwordField: 'password'
+  },
+  (username, password, done) => {
+    console.log(`Username: ${username} Password: ${password}`);
+    users.findOne({
+      username: username
+    }).then(user => {
+      if (!user) {
+        return done(null, false, 'Incorrect username...');
+      }
+      if (!user.validatePassword(password)) {
+        return done(null, false, `Incorrect password...${password} ${user} ${user.password}`);
+      }
 
-    if (!user) {
-      console.log('incorrect username');
-      return callback(null, false, { message: 'Incorrect username.' });
-    }
-
-    if (!user.validatePassword(password)) {
-      console.log('incorrect password here');
-      return callback(null, false, { message: `${user}  ${user[0].password}  ${user.password}` });
-    }
-
-    console.log('finished');
-    return callback(null, user);
-  });
-}));
+      // console.log('Finished');
+      return done(null, user);
+    }).catch(err => {
+      done(err, false, { 'Error': err });
+    });
+  }));
 
 passport.use(new JWTStrategy({
   jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
