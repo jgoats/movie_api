5a1842719b4181f62554cea92ed8e2c5a546b4a0
Justin Goats <justingoats@yahoo.com>
2021-06-28 17:51:12 -0700

    commit to master

--- passport.js
+++ passport.js
@@ -22,7 +22,8 @@ passport.use(new localStrategy(
         return done(null, false, 'Incorrect username...');
       }
       if (!user.validatePassword(password)) {
-        return done(null, false, `Incorrect password...${user}`);
+        console.log('incorrect password');
+        return done(null, false, { message: 'Incorrect password.' });
       }
 
       // console.log('Finished');
