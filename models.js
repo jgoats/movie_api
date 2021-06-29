6d7d206356ef600439e0c5d55e804aacf12911f1
Justin Goats <justingoats@yahoo.com>
2021-06-28 20:39:29 -0700

    changed bcrypt package to bcryptjs package

--- models.js
+++ models.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 let movieSchema = mongoose.Schema({
   title: { type: String, required: true },
